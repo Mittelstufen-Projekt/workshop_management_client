b980c37317130603e22276b769f6eb6bb93f6d36
Jasha Meusel <j.meusel@neusta.de>
2024-04-17 11:08:25 +0200

    refacor

---
+++ src/models/project_model.rs
@@ -0,0 +1,13 @@
+//Change Later
+
+struct ProjectModel {
+    name: String,
+    client: String,
+    email: String,
+    tel: String,
+    project_start: String,
+    project_deadline: String,
+    calculated_costs: String,
+    current_costs: String,
+    mats: Vec<Material>,
+}
